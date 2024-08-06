4c34605956dda25beaa50c0d8ef0b2c68347e1fd
Abdullah Babrahem <aboodi_salem1@outlook.com>
2024-08-06 18:02:27 +0300

    Delete product form HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach((cartItem) => {
   });
   
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Wednesday, June 15
       </div>
@@ -100,5 +100,9 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
+
+      const container = document.querySelector(`.js-cart-container-${productId}`);
+
+      container.remove();
     });
   });
